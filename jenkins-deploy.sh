124b6d2daca735ccfdcb86f3f9e96dab8c15c779
Gagandeep Singh <gagandeep.singh.6895@protonmail.com>
2022-09-15 14:13:35 +0530

    refactor

--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/bash
 
+# Exit when any command fails
+set -e
+
+# Keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# Echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+
 while getopts h:p:ts flag
 do
     case "$flag" in
